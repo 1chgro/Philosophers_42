9f232d70b81542eb242b6f8d84b105ff8727e4dc
Omar Lachguer <olachgue@e3r5p2.1337.ma>
2025-03-01 16:12:42 +0000

    init philos

--- mandatory/philo.c
+++ mandatory/philo.c
@@ -2,63 +2,84 @@
 
 void init_args(char **av, t_table *table)
 {
-    table->num_of_philos = ft_atoi(av[1]);
-    table->time_to_die = ft_atoi(av[2]);
-    table->time_to_eat = ft_atoi(av[3]);
-    table->time_to_sleep = ft_atoi(av[4]);
-    table->must_eat_count = -1;
-    if (av[5])
-        table->must_eat_count = ft_atoi(av[5]);
-        
+	table->num_of_philos = ft_atoi(av[1]);
+	table->time_to_die = ft_atoi(av[2]);
+	table->time_to_eat = ft_atoi(av[3]);
+	table->time_to_sleep = ft_atoi(av[4]);
+	table->must_eat_count = -1;
+	if (av[5])
+		table->must_eat_count = ft_atoi(av[5]);
+		
 }
 
 void init_forks(t_table *table)
 {
-    int i;
-    if (pthread_mutex_init(&table->meal_lock, NULL) != 0)
-        return ;
-    table->forks = malloc(sizeof(pthread_mutex_t) *(table->num_of_philos));
-    if (!table->forks)
-        return ;
-    i = 0;
-    while(i < table->num_of_philos)
-    {
-        if (pthread_mutex_init(&table->forks[i], NULL) != 0)
-        {
-            free(table->forks);
-            return ;
-        }
-        i++;
-    }
+	int i;
+	if (pthread_mutex_init(&table->meal_lock, NULL) != 0)
+		return ;
+	table->forks = malloc(sizeof(pthread_mutex_t) *(table->num_of_philos));
+	if (!table->forks)
+		return ;
+	i = 0;
+	while(i < table->num_of_philos)
+	{
+		if (pthread_mutex_init(&table->forks[i], NULL) != 0)
+		{
+			free(table->forks);
+			return ;
+		}
+		i++;
+	}
 }
 
 void init_philos(t_table *table)
 {
-    
+	int	i;
+
+	table->philos = malloc(sizeof(t_philo) * table->num_of_philos);
+	if (!table->philos)
+	{
+		free(table->forks);
+		return ;
+	}
+	i = 0;
+	while (i < table->num_of_philos)
+	{
+		table->philos[i].id = i;
+		table->philos[i].num_of_meals = 0;
+		table->philos[i].last_meal = 0;
+		table->philos[i].is_dead = 0;
+		table->philos[i].is_eating = 0;
+		table->philos[i].is_sleeping = 0;
+		table->philos[i].left_fork = &table->forks[i];
+		table->philos[i].right_fork = &table->forks[(i + 1) % table->num_of_philos];
+		i++;
+	}
 }
+
 
 void init_all(char **av, t_table *table)
 {
-    init_args(av, table);
-    init_forks(table);
-    init_philos(table);
+	init_args(av, table);
+	init_forks(table);
+	init_philos(table);
 }
 void *fun(void *arg)
 {
-    printf("hello\n");
-    return (void *)0;
+	printf("hello\n");
+	return (void *)0;
 }
 
 int main(int ac, char **av)
 {
-    t_table table;
+	t_table table;
 
-    if (ac != 5 && ac != 6)
-        return (ft_putstr_fd("Error: Wrong num of args\n", 2), 1);
-    if (!check_input(av))
-        return (1);
-    init_all(av, &table);
+	if (ac != 5 && ac != 6)
+		return (ft_putstr_fd("Error: Wrong num of args\n", 2), 1);
+	if (!check_input(av))
+		return (1);
+	init_all(av, &table);
 
 
-    return (0);
+	return (0);
 }
--- mandatory/philo.h
+++ mandatory/philo.h
@@ -11,31 +11,31 @@
 
 typedef struct s_philo
 {
-    int id;
-    pthread_t thread;
-    pthread_mutex_t *right_fork;
-    pthread_mutex_t *left_fork;
-    int is_dead;
-    int is_sleeping;
-    int is_eating;
-    int last_meal;
-    int num_of_meals;
+	int id;
+	pthread_t thread;
+	pthread_mutex_t *right_fork;
+	pthread_mutex_t *left_fork;
+	int is_dead;
+	int is_sleeping;
+	int is_eating;
+	int last_meal;
+	int num_of_meals;
 }t_philo;
 
 
 typedef struct s_table
 {
-    int num_of_philos;
-    int time_to_die;
-    int time_to_eat;
-    int time_to_sleep;
-    int must_eat_count;
-    int death_flag;
-    size_t start_time;
-    pthread_mutex_t meal_lock;
-    // pthread_mutex_t sleep_lock;
-    pthread_mutex_t *forks;
-    t_philo *philos;
+	int num_of_philos;
+	int time_to_die;
+	int time_to_eat;
+	int time_to_sleep;
+	int must_eat_count;
+	int death_flag;
+	size_t start_time;
+	pthread_mutex_t meal_lock;
+	// pthread_mutex_t sleep_lock;
+	pthread_mutex_t *forks;
+	t_philo *philos;
 }   t_table;
 
 void	ft_putstr_fd(char *s, int fd);
