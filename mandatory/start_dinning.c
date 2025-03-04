a8315a30e85d650c61db83f17ee34cf51c12af5a
Omar Lachguer <olachgue@e3r1p9.1337.ma>
2025-03-04 00:49:30 +0000

    .

--- mandatory/start_dinning.c
+++ mandatory/start_dinning.c
@@ -21,9 +21,45 @@ void	*philo_routine(void *arg)
 	}
 	return (NULL);
 }
-int check_death(t_table *table, int i)
+int	check_death(t_table *table, int i)
 {
+	pthread_mutex_lock(&table->meal_lock);
+	if (!table->philos[i].is_eating && 
+		(time_get() - table->philos[i].last_meal) > 
+		(size_t)table->time_to_die)
+	{
+		printf("%zu %d %s\n", 
+			time_get() - table->start_time, 
+			table->philos[i].id, DEAD_MSG);
+		table->death_flag = 1;
+		pthread_mutex_unlock(&table->meal_lock);
+		return (1);
+	}
+	pthread_mutex_unlock(&table->meal_lock);
+	return (0);
+}
 
+int	check_all_ate(t_table *table)
+{
+	int	i;
+	int	all_ate;
+	
+	if (table->must_eat_count == -1)
+		return (0);
+	
+	all_ate = 1;
+	i = 0;
+	while (i < table->num_of_philos)
+	{
+		pthread_mutex_lock(&table->meal_lock);
+		if (table->philos[i].num_of_meals < table->must_eat_count)
+			all_ate = 0;
+		pthread_mutex_unlock(&table->meal_lock);
+		if (!all_ate)
+			break ;
+		i++;
+	}
+	return (all_ate);
 }
 
 void	*monitor(void *arg)
@@ -48,7 +84,6 @@ void	*monitor(void *arg)
 			pthread_mutex_unlock(&table->meal_lock);
 			return (NULL);
 		}
-		usleep(1000);
 	}
 	return (NULL);
 }
